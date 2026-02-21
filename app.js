c1c0cbbe25324225af0abafb718af2155f799482
Shaini Jayakody <shainijayakody8@gmail.com>
2026-02-22 00:20:15 +0530

    Added file reading example

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Lab 04 - NodeJS Setup Successful");
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
